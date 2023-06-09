9c2aab7cb02778496a55863ec7d0aa9338b75f27
George Kortsaridis <gkortsaridis@gmail.com>
2023-06-09 17:10:57 +0100

    Starter Code

---
+++ app/src/main/java/com/example/mobilecomputing_23_lesson0401/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobilecomputing_23_lesson0401
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
